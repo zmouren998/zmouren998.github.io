72a8c454b30a540e44f7bcc4f46192e7cb0e8511
zmouren998 <guanghua199824@gmail.com>
2025-03-26 18:10:40 +0800

    Site updated: 2025-03-26 18:10:39

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/03/25/hello-world/"];function toRandomPost(){
+var posts=["2025/03/26/boost-learning/","2025/03/25/hello-world/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
